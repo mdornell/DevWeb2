bf575cd2bfa888997c0eca93ebcd792bff71279b
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2024-09-09 13:33:37 -0300

    Estrutura inicial

---
+++ VideoLocadora/src/main/java/model/domain/Ator.java
@@ -0,0 +1,22 @@
+package model.domain;
+
+public class Ator {
+    private String nome;
+    
+    public Ator(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return this.nome;
+    }
+}
